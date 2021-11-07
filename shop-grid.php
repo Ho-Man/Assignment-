3838c22cb9c10d2243f40ed50f837d73d1d8934f
Man-Ho <manhmgcc19247@fpt.edu.vn>
2021-11-07 10:20:12 +0700

    up9

--- shop-grid.php
+++ shop-grid.php
@@ -50,12 +50,12 @@
     <!-- Hero Section End -->
 
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="img/backgroundvinyl.jpg">
+    <section class="breadcrumb-section set-bg" data-setbg="img/Background.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
-                        <h2>Vinyl Shop</h2>
+                        <h2>Pillow Shop</h2>
                         <div class="breadcrumb__option">
                             <a href="?page=content">Home</a>
                             <span>Shop</span>
