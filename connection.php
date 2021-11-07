31f23082bfcf93831b1f0189b84609bf71a4f041
Man-Ho <manhmgcc19247@fpt.edu.vn>
2021-11-07 11:47:13 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("postgres://eqsswfxaphixgx:79c5717c086ecfdb796105255f2a0f48e0ec8b5c2278c2e680b24bb8689abc65@ec2-54-145-139-208.compute-1.amazonaws.com:5432/d200jeu77acrei");
+$conn = pg_connect("postgres://zvrfuztpdwgnze:098ada228f1a82c1b2c6456e81870d1debc84a91a76e660db7c684d3d0d45cdc@ec2-35-169-49-157.compute-1.amazonaws.com:5432/ddd0a0ev0ed4tn");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
