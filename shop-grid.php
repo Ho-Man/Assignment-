976917baaee96d7dee035f7e44f0b150bf22a560
Man-Ho <manhmgcc19247@fpt.edu.vn>
2021-11-08 11:05:32 +0700

    Update shop-grid.php

--- shop-grid.php
+++ shop-grid.php
@@ -327,7 +327,7 @@
 
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="img/<?php echo $row['Pro_image'] ?>">
+                                <div class="product__item__pic set-bg" data-setbg="img/<?php echo $row['pro_image'] ?>">
                                     <ul class="product__item__pic__hover">
                                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                         <li><a href="#"><i class="fa fa-retweet"></i></a></li>
