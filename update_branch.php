621bc4df3817570d01eeee3f66be930b04575a68
Man-Ho <manhmgcc19247@fpt.edu.vn>
2021-11-12 09:25:56 +0700

    Update update_branch.php

--- update_branch.php
+++ update_branch.php
@@ -1,7 +1,7 @@
 
 
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="img/Background.jpg.jpg">
+    <section class="breadcrumb-section set-bg" data-setbg="img/Background.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
