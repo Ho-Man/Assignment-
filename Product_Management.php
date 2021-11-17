cb2e41d45d52d3fe0ee8196c3328cc93ffda535a
Man-Ho <manhmgcc19247@fpt.edu.vn>
2021-11-17 17:01:18 +0700

    Update Product_Management.php

--- Product_Management.php
+++ Product_Management.php
@@ -91,7 +91,7 @@
                                 <tr>
                                     <th class="shoping__product">Products</th>
                                     <th class="shoping__product">Category</th>
-                                   
+                                    <th class="shoping__product">Branch  </th>
                                     <th>Price</th>
                                     <th>Quantity</th>
                                     <th><a href="?page=addp">Add</a></th>
@@ -124,14 +124,14 @@
                                 ?>
                             <tbody>
                             <?php //
-                                 if(isset($_GET['id'])){
+                                  if(isset($_GET['id'])){
                                     $id=$_GET['id'];
-                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
-                                    from product, category where product.cat_id = category.cat_id and '$id'=category.cat_id ");
+                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, ranch.branch_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
+                                    from product, category, branch where product.cat_id = category.cat_id and product.branch_id=branch.branch_id '$id'=category.cat_id ");
             
                                 }else{
-                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
-                                    from product, category where product.cat_id = category.cat_id ");
+                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name,branch.branch_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
+                                    from product, category, branch where product.cat_id = category.cat_id and product.branch_id=branch.branch_id ");
                                 }
                                 while($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) { 
                                     ?>
@@ -146,6 +146,9 @@
                                     <td class="shoping__cart__item">
                                         
                                         <h5><?php echo $row["cat_name"]; ?></h5>
+                                    </td>
+                                    <td class="shoping__cart__name">
+                                        <?php echo $row["branch_name"]; ?>
                                     </td>
                                     <td class="shoping__cart__price">
                                         $<?php echo $row["price"]; ?>
